1a73eed2133096deea90b0fb0b4e9c93f58b1c70
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2019-07-25 14:24:05 +0300

    Apply: Update user's views to show assets assigned to other assets #6489

--- app/Http/Controllers/ViewAssetsController.php
+++ app/Http/Controllers/ViewAssetsController.php
@@ -55,7 +55,8 @@ class ViewAssetsController extends Controller
         $userlog = $user->userlog->load('item', 'user', 'target');
 
         if (isset($user->id)) {
-            return view('account/view-assets', compact('user', 'userlog'));
+            return view('account/view-assets', compact('user', 'userlog'))
+                ->with('settings', Setting::getSettings());
         } else {
             // Prepare the error message
             $error = trans('admin/users/message.user_not_found', compact('id'));
